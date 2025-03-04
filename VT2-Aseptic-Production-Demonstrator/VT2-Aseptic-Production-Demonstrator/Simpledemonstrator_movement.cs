f59f44471e98414cec2de3a4687d0ed333100b30
SilasStensgaard <Silasstensgaard@gmail.com>
2025-03-04 16:51:22 +0100

    Added a filling queue

--- VT2-Aseptic-Production-Demonstrator/VT2-Aseptic-Production-Demonstrator/Simpledemonstrator_movement.cs
+++ VT2-Aseptic-Production-Demonstrator/VT2-Aseptic-Production-Demonstrator/Simpledemonstrator_movement.cs
@@ -1,5 +1,6 @@
 ﻿using PMCLIB;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,14 @@ namespace VT2_Aseptic_Production_Demonstrator
         private static XBotCommands _xbotCommand = new XBotCommands();
         WaitUntilTriggerParams CMD_params = new WaitUntilTriggerParams();
         int selector = 2;
+
+        //Creating lists for the station queues
+        public List<int> filling_queue = new List<int>() {1,2,3,4,1,2,3,4};
+
+        //Creating lists for the physical queues on the table
+        public List<int> physical_filling_queue = new List<int>();
+
+        public bool filling_running = false;
 
         public void initial_pos(int[] XIDs)
         {
@@ -41,6 +50,7 @@ namespace VT2_Aseptic_Production_Demonstrator
             bool finished = false;
             while(!finished)
             {
+                
                 XBotStatus status = _xbotCommand.GetXbotStatus(XIDs[0]);
                 Enum state1 = status.XBOTState;
                 status = _xbotCommand.GetXbotStatus(XIDs[1]);
@@ -49,7 +59,7 @@ namespace VT2_Aseptic_Production_Demonstrator
                 Enum state3 = status.XBOTState;
                 status = _xbotCommand.GetXbotStatus(XIDs[3]);
                 Enum state4 = status.XBOTState;
-                if (state1.ToString() == "IDLE" & state2.ToString() == "IDLE" & state3.ToString() == "IDLE" & state4.ToString() == "IDLE")
+                if (Convert.ToInt32(state1) == 3 & Convert.ToInt32(state2) == 3 & Convert.ToInt32(state3) == 3 & Convert.ToInt32(state4) == 3)
                 {
                     finished = true;
                     break;
@@ -57,27 +67,51 @@ namespace VT2_Aseptic_Production_Demonstrator
             }
 
         }
-        public bool Filling_occupied(int XID, bool run_cmd)
+
+        public void updating_filling_queue(string action, int? ID = null)
         {
-            if (!run_cmd)
+            double[] queue_posX = {0.420, 0.420, 0.300, 0.300};
+            double[] queue_posY = {0.780, 0.900, 0.900 ,0.780 };
+
+            if (action == "remove")
+            {
+                physical_filling_queue.Remove(ID.Value);
+            }
+            else if (action == "add")
+            {
+                physical_filling_queue.Add(ID.Value);
+            }
+            else if (action == "update")
+            {
+                //Here we update the physical positions in the queue
+                for (int i = 0; i < physical_filling_queue.Count; i++)
+                {
+                    _xbotCommand.LinearMotionSI(0, physical_filling_queue[i], POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.YTHENX, queue_posX[i], queue_posY[i], 0, 0.5, 2);
+                }
+            }
+            else return;
+                
+        }
+
+        public void Filling_occupied(int XID)
+        {
+            if (!filling_running)
             {
                 _xbotCommand.LinearMotionSI(0, XID, POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.660, 0.060, 0, 0.5, 2);
                 _xbotCommand.LinearMotionSI(0, XID, POSITIONMODE.ABSOLUTE, LINEARPATHTYPE.XTHENY, 0.060, 0.780, 0, 0.5, 2);
-                run_cmd = true;
-                Console.WriteLine("Filling begun");
-                return run_cmd;
+                filling_running = true;
+                
+
             }
-            else if (run_cmd)
+            else if (filling_running)
             {
                 XBotStatus status = _xbotCommand.GetXbotStatus(XID);
                 Enum state = status.XBOTState;
                 if (state.ToString() == "IDLE")
                 {
-                    run_cmd = false;
-                    return run_cmd;
+                    filling_running = false;
                 }
             }
-            return run_cmd; // Ensure all code paths return a value
         }
 
 
@@ -104,11 +138,38 @@ namespace VT2_Aseptic_Production_Demonstrator
 
                 case '1':
 
+                    //Moving to initial positions
                     initial_pos(xbot_ids);
-                    int current_filling = 0;
-                    bool run_cmd = false;
 
-                    Filling_occupied(xbot_ids[0], run_cmd);
+                    //Defining varaibles
+                    int current_filling = 0;
+                    
+
+                    //Adding the relevant shuttles to the filling queue
+                    for (int i = 0; i < xbot_ids.Length; i++)
+                    {
+                        physical_filling_queue.Add(xbot_ids[i]);
+                    }
+
+                    while (true)
+                    {
+                        updating_filling_queue("update"); //Update the physical queue
+                        if (!filling_running & filling_queue.Count > 0) //If filling is not running AND the filling_queue is not empty
+                        {
+                            Filling_occupied(filling_queue[0]); //Start the filling process with the ID that is first in the filling_queue
+                            current_filling = filling_queue[0]; //Save that ID for checking later
+                            updating_filling_queue("remove", filling_queue[0]); //Remove that specific ID from the physical_filling_queue
+                            filling_queue.RemoveAt(0); //Remove the specific ID from the filling_queue aswell
+                            updating_filling_queue("add", current_filling); //Inserting the shuttle into the filling queue again, just as a test
+                        }
+                        else
+                        {
+                            Filling_occupied(current_filling); //Just checking if this ID is still moving
+                        }
+
+                    }
+
+                    
                     
 
 
