e0b1e2d7a3c5c658922ffffd5f286cdcae0b6620
SilasStensgaard <Silasstensgaard@gmail.com>
2025-04-16 09:36:26 +0200

    Fixed the missing solution for command handler

---
+++ CommandHandler/CommandHandler/MQTTSubscriber.cs
@@ -0,0 +1,81 @@
+﻿using MQTTnet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MQTTnet.Client; 
+using System.Threading.Tasks;
+using MQTTnet.Protocol;
+using MQTTnet.Formatter;
+
+namespace CommandHandlerNode
+{
+    class MQTTSubscriber
+    {
+        private readonly IMqttClient _mqttClient;
+        private readonly MqttClientOptions _options;
+        private readonly string _topic;
+
+        // Define the MessageReceived event
+        public event Action<string, string> MessageReceived;
+
+
+        public MQTTSubscriber(string broker, int port) //also the topic here is not in Jeppes code
+        {
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+            //_topic = topic; //He is commented out here
+
+            _options = new MqttClientOptionsBuilder()
+                .WithTcpServer(broker, port)
+                .WithCleanSession()
+                .WithProtocolVersion(MqttProtocolVersion.V500)  // Set to MQTT v5.0
+                .Build();
+
+            _mqttClient.ApplicationMessageReceivedAsync += MessageReceivedHandler;
+            //_mqttClient.ConnectedAsync += ConnectedHandler; //Maybe delete
+            _mqttClient.DisconnectedAsync += DisconnectedHandler;
+        }
+
+        public async Task StartAsync()
+        {
+            await _mqttClient.ConnectAsync(_options);
+        }
+
+        public async Task StopAsync()
+        {
+            await _mqttClient.DisconnectAsync();
+        }
+
+        public async Task SubscribeAsync(string topic)
+        {
+            await _mqttClient.SubscribeAsync(topic, MqttQualityOfServiceLevel.AtLeastOnce);
+            Console.WriteLine($"Subscribed to topic: {topic}");
+        }
+
+        private async Task MessageReceivedHandler(MqttApplicationMessageReceivedEventArgs e)
+        {
+            string topic = e.ApplicationMessage.Topic;
+            string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+
+            Console.WriteLine($"Received message on topic '{topic}': {payload}");
+
+            // Raise the MessageReceived event
+            MessageReceived?.Invoke(topic, payload);
+        }
+
+        private async Task ConnectedHandler(MqttClientConnectedEventArgs e)
+        {
+            Console.WriteLine("Connected to MQTT broker.");
+            await _mqttClient.SubscribeAsync(_topic, MqttQualityOfServiceLevel.AtLeastOnce);
+            Console.WriteLine($"Subscribed to topic: {_topic}");
+        }
+
+        private async Task DisconnectedHandler(MqttClientDisconnectedEventArgs e)
+        {
+            Console.WriteLine("Disconnected from MQTT broker. Reconnecting in 5 seconds...");
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await _mqttClient.ConnectAsync(_options);
+        }
+    }
+}
---
+++ CommandHandler/CommandHandler/Queue_Class.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Protocol;
+using MQTTnet.Formatter;
+using System.Text.Json;
+
+namespace CommandHandlerNode
+{
+    class QueueClass
+    {
+
+        public List<int> physicalQueue = new List<int>();  //Making a queue list
+        private string queueName_string;
+
+        private double[] queuePosX; //Accepting an array of x positions
+        private double[] queuePosY; //Accepting an array of y positions
+
+        public int queueSize; //Size of the queue is dependent on how many queue positions are given
+
+        private MQTTPublisher mqttPublisher;
+        string brokerIP = "localhost";
+        //string brokerIP = "172.20.66.135";
+        int port = 1883;
+
+        public QueueClass()
+        {
+            InitializeMqttPublisher();
+        }
+
+        private async void InitializeMqttPublisher()
+        {
+            mqttPublisher = new MQTTPublisher(brokerIP, port);
+            await mqttPublisher.StartAsync();
+        }
+
+        //----------------------------------Give Name to Queue----------------------------------------
+        //--------------------------------------------------------------------------------------------
+        public string queueName
+        {
+            get { return queueName_string; }
+
+            set
+            {
+                queueName_string = value;
+            }
+
+        }
+
+        //----------------------------------Functions for Adding and removing to Queue----------------
+        //--------------------------------------------------------------------------------------------
+
+        public void addIDToList(int IDToAdd)
+        {
+            if (physicalQueue.Contains(IDToAdd))
+            {
+                //Console.WriteLine($"ID {IDToAdd} is already in the queue.");
+                return;  // Exit the function early
+            }
+
+            if (physicalQueue.Count < queueSize)
+            {
+                physicalQueue.Add(IDToAdd);
+            }
+            else
+            {
+                return; //Console.WriteLine("Physicla queue is full, unable to add");
+            }
+            
+        }
+
+        public void removeIDFromList(int IDToRemove)
+        {
+            if (physicalQueue.Count > 0)
+            {
+                physicalQueue.Remove(IDToRemove);
+            }
+            else
+            {
+                return; //Console.WriteLine("No shuttle to remove");
+            }
+            
+        }
+
+        //----------------------------------Set Queue Positions and Direction-------------------------
+        //--------------------------------------------------------------------------------------------
+
+        public void passingStationQueuePositions(double[] queuePositionsX, double[] queuePositionsY)
+        {
+            queuePosX = queuePositionsX;
+            queuePosY = queuePositionsY;
+            queueSize = queuePositionsX.Length;
+        }
+
+
+        //----------------------------------Updating Queue--------------------------------------------
+        //--------------------------------------------------------------------------------------------
+
+        public async void updateQueuePositions() //When we get a bit further, this should instead just send coordinates to the path planner for each shuttle
+        {
+            if (physicalQueue.Count > 0)
+            {
+                for (int i = 0; i < physicalQueue.Count; i++)
+                {
+                    double[] queuePOS = [queuePosX[i], queuePosY[i]];
+                    var message = JsonSerializer.Serialize(queuePOS);
+                    Console.WriteLine($"Sending target position {queuePOS} to Xbot{physicalQueue[i]} ");
+                    await mqttPublisher.PublishMessageAsync($"AAU/Fiberstræde/Building14/FillingLine/Stations/Acopos6D/Xbots/Xbot{physicalQueue[i]}/TargetPosition", message);
+                }
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+
+    }
+}
